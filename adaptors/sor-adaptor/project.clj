3e4cc387b5fc552e27522d24f7e1fcd66077653f
Per Magnus Petersen <per.petersen@cgi.com>
2018-08-13 11:55:54 +0200

    Resource 1.sor added

--- adaptors/sor-adaptor/project.clj
+++ adaptors/sor-adaptor/project.clj
@@ -3,7 +3,8 @@
    :dependencies [[org.clojure/clojure "1.8.0"]
                   [clojure-csv/clojure-csv "2.0.1"]
                   [com.taoensso/nippy "2.14.0"]
-                  [metosin/compojure-api "1.1.11"]]
+                  [metosin/compojure-api "1.1.11"]
+                  [environ "1.1.0"]]
    :ring {:handler sor-adaptor.handler/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
--- adaptors/sor-adaptor/src/sor_adaptor/import.clj
+++ adaptors/sor-adaptor/src/sor_adaptor/import.clj
@@ -1,40 +1,57 @@
 (ns sor-adaptor.import
   (:require [clojure-csv.core :refer [parse-csv]]
             [clojure.data :refer [diff]]
+            [environ.core :refer [env]]
+            [clojure.java.io :as io]
             [taoensso.nippy :as nippy]))
 
-#_(defonce csv (slurp "http://filer.nsi.dk/sor/data/sor/sorcsv/v_1_2_1_19/sor.csv" :encoding "ISO-8859-1"))
-#_(spit "data/cor.csv" csv)
+(defonce sor-1 (nippy/thaw-from-file (io/resource "1.sor")))
 
-#_(defonce sor-csv (slurp "data/sor.csv"))
-#_(def parsed (parse-csv sor-csv :delimiter \;))
+(def sor-csv-file (if-let [e (env :sor-csv)]
+                    e
+                    "http://filer.nsi.dk/sor/data/sor/sorcsv/v_1_2_1_19/sor.csv"))
 
 (defn csv-map
   "ZipMaps header as keys and values from lines."
   [head, lines]
   (map #(zipmap (map keyword head) %1) lines))
 
-#_(time (def sor-map (group-by :SOR-kode (csv-map (first parsed) (rest parsed)))))
+(defn group-by-sor [sor-seq]
+  (reduce (fn [a r]
+            (assoc a (:SOR-kode r) r))
+          {}
+          sor-seq))
 
-(defn reload-sor-csv-file [base]
+(defn- fetch-sor-csv-file
+  []
+  (slurp sor-csv-file :encoding "ISO-8859-1"))
+
+(defn reload-sor-csv-file [base csv]
   (println "Reload sor csv file ...")
-  (let [csv (slurp "http://filer.nsi.dk/sor/data/sor/sorcsv/v_1_2_1_19/sor.csv"
-                   :encoding "ISO-8859-1")
-        _ (println "  Parse CSV file ...")
+  (let [_ (println "  Parse CSV file ...")
         parsed (parse-csv csv :delimiter \;)
         _ (println "  Group CSV file ...")
-        sor-map (group-by :SOR-kode
-                          (csv-map (first parsed) (rest parsed)))]
+        sor-map (group-by-sor (csv-map (first parsed) (rest parsed)))]
     (println "  Diff reload sor csv file...")
     (diff sor-map base)))
 
-
-#_(nippy/freeze-to-file "data/0.sor" sor-map)
-
-(defonce sor-0 (nippy/thaw-from-file "data/0.sor"))
-
 (comment
-  (count sor-0)
-  (time (def reload-csv (reload-sor-csv-file sor-0)))
+  (count sor-1)
+  (first sor-1)
+
+  (time (def raw-file (fetch-sor-csv-file)))
+  (time (def parsed (parse-csv raw-file :delimiter \;)))
+  (time (def zm (csv-map (first parsed) (rest parsed))))
+  (keys (first zm))
+  (time (def gb (group-by-sor zm)))
+  (time (nippy/freeze-to-file "data/1.sor" gb))
+  (first gb)
+
+  (time (def reload-csv (reload-sor-csv-file sor-1 (fetch-sor-csv-file))))
   (let [d reload-csv]
-    [ (count (first d)) (count (second d)) (count (nth d 2))]))
+    [(count (first d)) (count (second d)) (count (nth d 2))]))
+
+
+
+
+
